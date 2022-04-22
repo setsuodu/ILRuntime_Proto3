11a85e886ba94aa3e151c7ba93f92af71613153c
setsuodu <339131001@qq.com>
2022-04-22 18:33:03 +0800

    写文件工具

---
+++ Unity/Assets/Scripts/FileHelper.cs
@@ -0,0 +1,80 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEditor;
+
+public class FileHelper
+{
+    public static void WriteText()
+    {
+        string content = "123456";
+        string fileName = "ABC";
+        string root = Environment.CurrentDirectory;
+        //string outPath = $"{root}/Assets/{fileName}.txt";
+        string outPath = $"./Assets/{fileName}.txt";
+        string srcPath = $"{root}/Assets/{fileName}.txt";
+
+        /*
+        // 创建
+        string srcContent = string.Empty;
+        bool srcExist = File.Exists(outPath);
+        Debug.Log($"{outPath}, exist={srcExist}");
+        if (srcExist == false)
+        {
+            // 没有文件，创建，写入。
+        }
+        else
+        {
+            // 有文件，先读取，新建一行写入。
+            srcContent = File.ReadAllText(outPath);
+            content = srcContent + "\n" + content;
+        }
+        byte[] data = Encoding.UTF8.GetBytes(content);
+        File.WriteAllBytes(outPath, data); //直接覆盖了
+        */
+
+        /*
+        // 读取
+        var fileExist = File.Exists(srcPath);
+        Debug.Log($"{srcPath}, exist={fileExist}");
+        if (fileExist == false)
+        {
+            return;
+        }
+        FileInfo file = new FileInfo(srcPath);
+        StreamReader sr = file.OpenText();
+        string fileStr = sr.ReadToEnd();
+        sr.Close();
+        sr.Dispose();
+        Debug.Log($"读取：{fileStr}");
+        */
+
+        /*
+        // 删除
+        var exist = File.Exists(outPath);
+        Debug.Log($"{outPath}, exist={exist}");
+        if (exist)
+        {
+            File.Delete(outPath);
+            Debug.Log($"exist={exist}");
+        }
+        */
+
+        // 修改
+
+
+        //AssetDatabase.Refresh();
+    }
+
+    public static void WriteBytes(byte[] data)
+    {
+        Debug.Log($"WriteBytes: data={data.Length}");
+
+        string fileName = "byte[]对比";
+        string root = Environment.CurrentDirectory;
+        string filePath = $"{root}/Assets/{fileName}.txt";
+        File.WriteAllBytes(filePath, data); //直接覆盖了
+    }
+}
